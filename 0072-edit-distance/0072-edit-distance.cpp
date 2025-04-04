d3ed57ba8452a6b62b397b430a45e0071a41d3f0
Jalaj Dabas <jalajdabas4@gmail.com>
2025-04-04 23:03:50 +0530

    Time: 2 ms (93.09%), Space: 13.5 MB (23.75%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -44,6 +44,26 @@ public:
         }
         return dp[n][m];
     }
+
+    int space(int n, int m, string s1, string s2) {
+        vector<int> prev(m+1, 0);
+        vector<int> curr(m+1, 0);
+        for(int j=0; j<=m; j++) prev[j] = j;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                
+                if(s1[i-1] == s2[j-1]){
+                    curr[j] = prev[j-1];
+                }
+                else{
+                    curr[j] = 1 + min(curr[j-1], min(prev[j], prev[j-1]));
+                }
+            }
+            prev = curr;
+        }
+        return prev[m];
+    }
     int minDistance(string word1, string word2) {
         int n = word1.length();
         int m = word2.length();
