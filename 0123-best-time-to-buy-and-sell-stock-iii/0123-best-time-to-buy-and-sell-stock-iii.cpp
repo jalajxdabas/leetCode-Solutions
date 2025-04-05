451397afe604576d00b5fcb4a97a6fe49b188d28
Jalaj Dabas <jalajdabas4@gmail.com>
2025-04-05 22:13:02 +0530

    Time: 418 ms (33.47%), Space: 211.4 MB (44.74%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -28,10 +28,41 @@ public:
         }
         return dp[i][buy][cap] = profit;
     }
+
+    int tab(vector<int> &prices){
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+
+        // for(int i=0; i<n; i++){
+        //     for(int buy=0; buy<2; buy++){
+        //         dp[i][buy][0] = 0;
+        //     }
+        // }
+
+        for(int i=n-1; i>=0; i--){
+            for(int buy=0; buy<2; buy++){
+                for(int cap=1; cap<3; cap++){
+                    int profit = 0;
+                    if(buy){
+                        profit = max(dp[i+1][1][cap], (-prices[i] + dp[i+1][0][cap]));
+                    }
+                    else{
+                        profit = max(dp[i+1][0][cap], (+prices[i]+dp[i+1][1][cap-1]));
+                    }
+                    dp[i][buy][cap] = profit;
+                }
+            }
+        }
+        return dp[0][1][2];
+
+        
+    }
     int maxProfit(vector<int>& prices) {
         // return recur(0, 1, 2, prices);
-        int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
-        return memo(0, 1, 2, prices, dp);
+        // int n = prices.size();
+        // vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
+        // return memo(0, 1, 2, prices, dp);
+
+        return tab(prices);
     }
 };
