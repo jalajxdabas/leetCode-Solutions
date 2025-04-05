cd81d22b179cd1f67157828a8dc4f21c9518c0a7
Jalaj Dabas <jalajdabas4@gmail.com>
2025-04-05 21:35:59 +0530

    Time: 2 ms (31.17%), Space: 19.8 MB (20.7%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -27,11 +27,53 @@ public:
         }
         return dp[i][buy] = profit;
     }
+
+    int tab(vector<int> &prices){
+        int n = prices.size();
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        dp[n][0] = dp[n][1] = 0;
+
+        for(int i=n-1; i>=0; i--){
+            for(int buy=0; buy<2; buy++){
+                int profit = 0;
+                if(buy){
+                    profit = max((-prices[i] + dp[i+1][0]), dp[i+1][1]); 
+                }
+                else{
+                    profit = max(dp[i+1][0], (+prices[i] + dp[i+1][1]));
+                }
+                dp[i][buy] = profit;
+            }
+        }
+        return dp[0][1];
+    }
+
+    int space(vector<int> &prices){
+        int n = prices.size();
+        vector<int> next(2, 0), curr(2, 0);
+
+        for(int i=n-1; i>=0; i--){
+            for(int buy=0; buy<2; buy++){
+                int profit = 0;
+                if(buy){
+                    profit = max((-prices[i] + next[0]), next[1]); 
+                }
+                else{
+                    profit = max(next[0], (+prices[i] + next[1]));
+                }
+                curr[buy] = profit;
+            }
+            next = curr;
+        }
+        return next[1];
+    }
     int maxProfit(vector<int>& prices) {
         // return recur(0, buy, prices);
-        int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return memo(0, 1, prices, dp);
+        // int n = prices.size();
+        // vector<vector<int>> dp(n, vector<int>(2, -1));
+        // return memo(0, 1, prices, dp);
+
+        return tab(prices);
     }
 
 };
