4d08536f38160f820c796453ecd3d741a9baeef8
Jalaj Dabas <jalajdabas4@gmail.com>
2025-04-06 14:53:59 +0530

    Time: 3 ms (80.2%), Space: 14.1 MB (78.67%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,61 +1,17 @@
 class Solution {
 public:
-    int recur(int i, int prev, vector<int>& nums){
-        if(i == nums.size()) return 0;
-
-        int notTake = 0 + recur(i+1, prev, nums);
-        int take = 0;
-        if(prev == -1 || nums[i] > nums[prev]) take = 1 + recur(i+1, i, nums);
-        return max(take, notTake);
-    }
-
-    int memo(int i, int prev, vector<int> &nums, vector<vector<int>> &dp){
-        if(i == nums.size()) return 0;
-        if(dp[i][prev+1] != -1) return dp[i][prev+1];
-
-        int notTake = 0 + memo(i+1, prev, nums, dp);
-        int take = 0;
-        if(prev == -1 || nums[i] > nums[prev]) take = 1 + memo(i+1, i, nums, dp);
-        return dp[i][prev+1] = max(take, notTake);
-    }
-
-    int tab(vector<int> &nums){
-        int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
-
-        for(int i=n-1; i>=0; i--){
-            for(int prev = i-1; prev>=-1; prev--){
-                int notTake = 0 + dp[i+1][prev+1];
-                int take = 0;
-                if(prev == -1 || nums[i] > nums[prev]) take = 1 + dp[i+1][i+1];
-                dp[i][prev+1] = max(take, notTake);
-            }
-        }
-        return dp[0][0];
-    }
-
-    int tab2(vector<int> &nums){
-        int n = nums.size();
-        int ans = 0;
-
-        vector<int> dp(n, 1);
-        for(int i=0; i<n; i++){
-            for(int prev=0; prev<i; prev++){
-                if(nums[prev] < nums[i]) dp[i] = max(1+dp[prev], dp[i]);
-            }
-            ans = max(ans, dp[i]);
-        }
-        return ans;
-        
-    }
-
+    
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        // return recur(0, -1, nums);
-        // vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        // return memo(0, -1, nums, dp);
-        // return tab(nums);
+        vector<int> temp;
+        temp.push_back(nums[0]);
 
-        return tab2(nums);
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] > temp.back()) temp.push_back(nums[i]);
+            else{
+                int ind = (lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin());
+                temp[ind] = nums[i];
+            }
+        }
+        return temp.size();
     }
 };
