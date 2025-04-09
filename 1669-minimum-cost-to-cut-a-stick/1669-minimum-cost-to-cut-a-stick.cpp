00c8912aefab1d7844858ec187eb8d405c925048
Jalaj Dabas <jalajdabas4@gmail.com>
2025-04-09 23:00:25 +0530

    Time: 19 ms (82.94%), Space: 14 MB (46.93%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -22,14 +22,34 @@ public:
         }
         return dp[i][j] = mini;
     }
+
+    int tab(vector<int> &cuts, int n){
+        vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
+
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=n; j++){
+                if(i > j) continue;
+                int mini = 1e9;
+                for(int ind = i; ind<=j; ind++){
+                    int cost = (cuts[j+1]-cuts[i-1]) + dp[i][ind-1] + dp[ind+1][j];
+                    mini = min(cost, mini);
+                }
+                dp[i][j] = mini;
+            }
+        }
+        return dp[1][n];
+        
+    }
     int minCost(int n, vector<int>& cuts) {
         int c = cuts.size();
         cuts.push_back(n);
         cuts.push_back(0);
         sort(cuts.begin(), cuts.end());
-        vector<vector<int>> dp(c+1, vector<int>(c+1, -1));
+        // vector<vector<int>> dp(c+1, vector<int>(c+1, -1));
 
         // return recur(1, c, cuts);
-        return memo(1, c, cuts, dp);
+        // return memo(1, c, cuts, dp);
+
+        return tab(cuts, c);
     }
 };
