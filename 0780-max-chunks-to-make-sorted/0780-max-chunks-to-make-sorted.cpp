f99e1a886bb1ace215a544db572c02b0bf2e6abc
Jalaj Dabas <jalajdabas4@gmail.com>
2025-06-10 18:01:19 +0530

    Time: 0 ms (100%), Space: 9.1 MB (80.18%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -2,23 +2,15 @@ class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
         int n = arr.size();
-        vector<int> prefix(arr.begin(), arr.end());  //prefixMax
-        vector<int> suffix(arr.begin(), arr.end());  //suffixMax
-
-        for(int i=1; i<n; i++){
-            prefix[i] = max(prefix[i-1], prefix[i]);
-        }
-
-        for(int i=n-2; i>=0; i--){
-            suffix[i] = min(suffix[i], suffix[i+1]);
-        }
-
+        int curr = 0;
+        int original = 0;
         int count = 0;
-        for(int i=0; i<n; i++){
-            int pehleKaMax = i > 0 ? prefix[i-1] : -1;
-            int baadKaMin = suffix[i];
 
-            if(pehleKaMax < baadKaMin) count++;
+        for(int i=0; i<n; i++){
+            curr += arr[i];
+            original += i;
+
+            if(curr == original) count++;
         }
         return count;
     }
