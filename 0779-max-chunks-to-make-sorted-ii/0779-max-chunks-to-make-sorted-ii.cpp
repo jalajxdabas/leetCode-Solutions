64af01b150fa17cecb4b1c3f7d26660827a06b94
Jalaj Dabas <jalajdabas4@gmail.com>
2025-06-10 18:28:27 +0530

    Time: 0 ms (100%), Space: 16 MB (92.96%) - LeetHub

--- 0779-max-chunks-to-make-sorted-ii/0779-max-chunks-to-make-sorted-ii.cpp
+++ 0779-max-chunks-to-make-sorted-ii/0779-max-chunks-to-make-sorted-ii.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
-    int maxChunksToSorted(vector<int>& arr) {
-        int n = arr.size();
+    int maxChunksToSorted(vector<int>& nums) {
+        int n = nums.size();
+        stack<int>st;
 
-        vector<int> prefixMax(arr.begin(), arr.end());
-        vector<int> suffixMin(arr.begin(), arr.end());
-
-        for(int i=1; i<n; i++){
-            prefixMax[i] = max(prefixMax[i], prefixMax[i-1]);
-        }
-
-        for(int i=n-2; i>=0; i--){
-            suffixMin[i] = min(suffixMin[i], suffixMin[i+1]);
-        }
-
-        int count = 0;
         for(int i=0; i<n; i++){
-            int leftMax = i > 0? prefixMax[i-1] : -1;
-            int rightMin = suffixMin[i];
-
-            if(leftMax <= rightMin) count++;
+            int lastChunkEnd = nums[i];
+            while(!st.empty() && st.top() > nums[i]){
+                lastChunkEnd = max(st.top(), lastChunkEnd);
+                st.pop();
+            }
+            st.push(lastChunkEnd);
         }
-        return count;
+        return st.size();
     }
 };
