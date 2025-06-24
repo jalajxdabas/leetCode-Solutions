336da68547c970f8e2ea2ae6412ac844f982964b
Jalaj Dabas <jalajdabas4@gmail.com>
2025-06-24 22:34:06 +0530

    Time: 0 ms (100%), Space: 26.2 MB (66.86%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -8,11 +8,19 @@ public:
         while(low < high){
             int mid = low + (high-low)/2;
 
-            if (mid % 2 == 1) mid--;
+            bool isEven;
 
-            if(nums[mid] == nums[mid+1]) low = mid + 2;
+            if((high-mid)%2 == 0) isEven = true;
+            else isEven = false;
 
-            else high = mid;
+            if(nums[mid] == nums[mid+1]){
+                if(isEven) low = mid+2;
+                else high = mid-1;
+            }
+            else{
+                if(isEven) high = mid;
+                else low = mid+1;
+            }
 
         }
         return nums[low];
