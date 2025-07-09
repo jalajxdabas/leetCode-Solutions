2e1df24e2d0d345c8a309421eba45a5ae0102bce
Jalaj Dabas <jalajdabas4@gmail.com>
2025-07-09 13:57:47 +0530

    Time: 0 ms (100%), Space: 9.8 MB (48.59%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,11 +1,22 @@
+#include <string>
+#include <vector>
+
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+    bool isAnagram(std::string s, std::string t) {
+        if (s.size() != t.size()) return false;
 
+        int count[26] = {0};
 
-        if(s == t) return true;
-        return false;
+        for (size_t i = 0; i < s.size(); i++) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return false;
+        }
+
+        return true;
     }
 };
