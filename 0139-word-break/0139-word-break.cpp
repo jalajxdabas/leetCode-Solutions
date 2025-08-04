6e89de4e04047128b66e5733a82dfa3651822413
Jalaj Dabas <jalajdabas4@gmail.com>
2025-08-04 19:46:48 +0530

    Time: 28 ms (9.57%), Space: 22.9 MB (7.87%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    unordered_set<string> st;
+
+    int t[301];
+
+    bool solve(int i, string s){
+        if(i >= s.length()) return t[i] = true;
+        int n = s.length();
+
+        if(st.find(s) != st.end()) return t[i] = true;
+
+        if(t[i] != -1) return t[i];
+
+
+        for(int j = 1; j<= n; j++){
+            string temp = s.substr(i, j);
+
+            if(st.find(temp) != st.end() && solve(i+j, s)) return t[i] = true;
+        }
+        return t[i] = false;
+    }
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        for(auto &i: wordDict){
+            st.insert(i);
+        }
+
+        memset(t, -1, sizeof(t));
+
+        return solve(0, s);
+    }
+};
