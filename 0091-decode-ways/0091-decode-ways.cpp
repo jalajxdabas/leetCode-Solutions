42cb6809ce12ec53de2605e462b340561357f409
Jalaj Dabas <jalajdabas4@gmail.com>
2025-08-16 19:34:02 +0530

    Time: 3 ms (20.12%), Space: 11.1 MB (13.88%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int recur(int i, string s, int n){
+        if(i == n) return 1;
+        if(i > n) return 0;
+        if(s[i] == '0') return 0;;
+ 
+        int result = recur(i+1, s, n);
+
+        if((i+1 < n) && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6')) result += recur(i+2, s, n);
+
+        return result;
+    }
+
+    int memo(int i, string s, int n, vector<int> &dp){
+        if(i == n) return 1;
+        if(i > n) return 0;
+        if(s[i] == '0') return 0;;
+
+        if(dp[i] != -1) return dp[i];
+
+        int result = memo(i+1, s, n, dp);
+
+        if((i+1 < n) && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6')) result += memo(i+2, s, n, dp);
+
+        return dp[i] = result;
+    }
+    int numDecodings(string s) {
+        int n = s.length();
+        // if(s[0] == '0') return 0; 
+        // return recur(0, s, n);
+
+        vector<int> dp(n+1, -1);
+        return memo(0, s, n, dp);
+    }
+};
