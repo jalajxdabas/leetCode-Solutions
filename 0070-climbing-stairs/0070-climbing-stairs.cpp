d34a7d78d8cc09c7c874cba970b32a5cff21fbfa
Jalaj Dabas <jalajdabas4@gmail.com>
2025-08-16 15:43:14 +0530

    Time: 0 ms (100%), Space: 7.9 MB (73.1%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -26,6 +26,20 @@ public:
         return dp[n];
     }
 
+    int space(int n){
+        int prev1 = 1;
+        int prev2 = 1;
+        int curr;
+
+        for(int i=2; i<=n; i++){
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+
 
     int climbStairs(int n) {
         // return recur(n);
@@ -33,6 +47,8 @@ public:
         // vector<int> dp(n+1, -1);
         // return memo(n, dp);
 
-        return tab(n);
+        // return tab(n);
+
+        return space(n);
     }
 };
