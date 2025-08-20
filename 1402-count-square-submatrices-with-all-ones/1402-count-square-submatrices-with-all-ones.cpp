a3033f7c4120252f15942a692501eb8f57151738
Jalaj Dabas <jalajdabas4@gmail.com>
2025-08-20 21:03:59 +0530

    Time: 4 ms (73.98%), Space: 30.3 MB (70.36%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -40,15 +40,28 @@ public:
         int n = matrix[0].size();
         int result = 0;
 
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<vector<int>> dp(m, vector<int>(n, 0));
 
+
+        // for(int i=0; i<m; i++){
+        //     for(int j=0; j<n; j++){
+        //         if (matrix[i][j] == 1){
+        //             // result += recur(i, j, matrix);
+        //             result += memo(i, j, matrix, dp);
+        //         } 
+        //     }
+        // }
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if (matrix[i][j] == 1){
-                    // result += recur(i, j, matrix);
-                    result += memo(i, j, matrix, dp);
-                } 
+
+                if(i == 0 || j == 0) dp[i][j] = matrix[i][j];
+
+                else if(matrix[i][j] == 0) dp[i][j] = 0;
+
+                else dp[i][j] = 1 + min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1]));
+
+                result += dp[i][j];
             }
         }
 
