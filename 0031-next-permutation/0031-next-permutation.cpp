30a2ee9e7691773a4dd4fb852d64efaedc78c868
Jalaj Dabas <jalajdabas4@gmail.com>
2025-08-26 19:39:58 +0530

    Time: 0 ms (100%), Space: 15.8 MB (33.13%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -5,24 +5,27 @@ public:
         int n = nums.size();
 
         for(int i=n-2; i>=0; i--){
-            if(nums[i] < nums[i+1]){
-                index = i; 
+            if(nums[i+1] > nums[i]) {
+                index = i;
                 break;
             }
         }
 
-        if(index == -1){
+        if(index == -1) {
             reverse(nums.begin(), nums.end());
             return;
         }
 
-        for(int i=n-1; i > index; i--){
+
+        for(int i=n-1; i>index; i--){
             if(nums[i] > nums[index]){
                 swap(nums[i], nums[index]);
                 break;
             }
         }
+
         reverse(nums.begin()+index+1, nums.end());
         return;
+
     }
 };
