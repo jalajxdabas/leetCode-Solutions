da052cba961f65b1c1ebd452a9963400c3e79c01
Jalaj Dabas <jalajdabas4@gmail.com>
2025-08-26 20:05:52 +0530

    Time: 0 ms (100%), Space: 9.3 MB (33.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -21,11 +21,40 @@ public:
     }
 
 
+    int tab(int m, int n){
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        dp[0][0] = 1;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+
+                if(i == 0 && j == 0){
+                    dp[i][j] = 1;
+                    continue;
+                }
+                
+                int down = 0;
+                int right = 0;
+
+                if(i>0) down = dp[i-1][j];
+                if(j>0) right = dp[i][j-1];
+
+                dp[i][j] = down+right;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
+
+
     int uniquePaths(int m, int n) {
         
         // return recur(m-1, n-1);
 
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return memo(m-1, n-1, dp);
+        // vector<vector<int>> dp(m, vector<int>(n, -1));
+        // return memo(m-1, n-1, dp);
+
+        return tab(m, n);
     }
 };
