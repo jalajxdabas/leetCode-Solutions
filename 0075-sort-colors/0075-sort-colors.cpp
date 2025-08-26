380e9df94bd0f68e5fb0f8c2f01ac62bf2fef14c
Jalaj Dabas <jalajdabas4@gmail.com>
2025-08-26 19:42:37 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.08%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        //Dutch national flag problem (red, white, blue)
-        const char red=0, white=1, blue=2;
-        int l=0, m=0, r=nums.size()-1;// pointers to partition
-        while(m<=r){
-            switch(nums[m]){
-                case red:
-                    swap(nums[l], nums[m]);
-                    l++, m++;
-                    break;
-                case white:
-                    m++;
-                    break;
-                case blue:
-                    swap(nums[m], nums[r]);
-                    r--;
+        int n = nums.size();
+        int l = 0;
+        int r = n-1;
+        int m = 0;
+
+        while(m <= r){
+            if(nums[m] == 0){
+                swap(nums[l++], nums[m++]);
+            }
+            else if(nums[m] == 2){
+                swap(nums[r--], nums[m]);
+            }
+            else{
+                m++;
             }
         }
+        return;
     }
 };
