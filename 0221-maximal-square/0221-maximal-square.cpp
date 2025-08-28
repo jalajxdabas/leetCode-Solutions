1bb20282a8b9dd54af698c1760984a0805293231
Jalaj Dabas <jalajdabas4@gmail.com>
2025-08-28 16:18:19 +0530

    Time: 8 ms (65.84%), Space: 34.4 MB (5.66%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -28,6 +28,33 @@ public:
         return dp[row][col] = 1 + min(right, min(diag, down));
     }
 
+    int tab(vector<vector<char>> &matrix){
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int maxi = 0;
+
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+
+                int right = dp[i][j+1];
+                int diag = dp[i+1][j+1];
+                int down = dp[i+1][j];
+
+                if(matrix[i][j] == '1'){
+                    dp[i][j] = 1 + min(right, min(diag, down));
+
+                    maxi = max(maxi, dp[i][j]);
+                }
+                else dp[i][j] = 0;
+            }
+        }
+
+        return maxi*maxi;
+
+    }
+
 
     int maximalSquare(vector<vector<char>>& matrix) {
         int ans = 0;
@@ -36,16 +63,16 @@ public:
 
         vector<vector<int>> dp(m, vector<int>(n, -1));
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(matrix[i][j] == '1'){
-                    // ans = max(ans, solve(i, j, matrix));
-                    ans = max(ans, memo(i, j, matrix, dp));
+        // for(int i=0; i<m; i++){
+        //     for(int j=0; j<n; j++){
+        //         if(matrix[i][j] == '1'){
+        //             // ans = max(ans, solve(i, j, matrix));
+        //             ans = max(ans, memo(i, j, matrix, dp));
 
-                }
-            }
-        }
+        //         }
+        //     }
+        // }
 
-        return ans*ans;
+        return tab(matrix);
     }
 };
