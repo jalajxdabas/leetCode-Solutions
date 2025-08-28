40feb766f64418ccae81166b6de779f23ab8884a
Jalaj Dabas <jalajdabas4@gmail.com>
2025-08-28 12:54:10 +0530

    Time: 10 ms (55.76%), Space: 46.5 MB (11.73%) - LeetHub

--- 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.cpp
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.cpp
@@ -1,44 +1,31 @@
 class Solution {
 public:
-    void sortDiag(int row, int col, vector<vector<int>> &grid, bool asc){
-        int i = row;
-        int j = col;
-        int n = grid.size();
-
-        vector<int> temp;
-
-        while(i < n && j < n){
-            temp.push_back(grid[i][j]);
-            i++;
-            j++;
-        }
-
-        if(asc) sort(temp.begin(), temp.end());
-        else sort(temp.rbegin(), temp.rend());
-
-
-        i = row;
-        j = col;
-        int idx = 0;
-
-        while(i < n && j < n){
-            grid[i][j] = temp[idx];
-            i++;
-            j++;
-            idx++;
-        }
-
-        return;
-    }
     vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+        unordered_map<int, vector<int>> mp;
         int n = grid.size();
 
-        for(int row = 0; row<n; row++){
-            sortDiag(row, 0, grid, false);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                int diag = i-j;
+                mp[diag].push_back(grid[i][j]);
+            }
         }
 
-        for(int col = 1; col <n; col++){
-            sortDiag(0, col, grid, true);
+        for(auto &it: mp){
+            if(it.first >= 0){
+                sort(begin(it.second), end(it.second));
+            }
+            else{
+                sort(rbegin(it.second), rend(it.second));
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                int diag = i-j;
+                grid[i][j] = mp[diag].back();
+                mp[diag].pop_back();
+            }
         }
 
         return grid;
