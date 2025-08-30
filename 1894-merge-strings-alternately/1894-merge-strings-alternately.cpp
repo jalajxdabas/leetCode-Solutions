72d7f8fd008c6afa1aa487700ee462ad3df35de0
Jalaj Dabas <jalajdabas4@gmail.com>
2025-08-30 16:26:38 +0530

    Time: 4 ms (30.76%), Space: 8.4 MB (63.23%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans = "";
+        int m = word1.length();
+        int n = word2.length();
+
+        int i=0, j=0; 
+
+        while(i < m && j < n){
+            ans += word1[i++];
+            ans += word2[j++];
+        }
+
+        while(i < m){
+            ans += word1[i++];
+        }
+
+        while(j < n){
+            ans += word2[j++];
+        }
+
+        return ans;
+    }
+};
