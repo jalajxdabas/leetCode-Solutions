6992b0a42d167927baaaebfe2c36d8338d392ea3
Jalaj Dabas <jalajdabas4@gmail.com>
2025-08-30 15:48:02 +0530

    Time: 3 ms (64.2%), Space: 18.4 MB (66.18%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,40 +8,45 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class compare{
-    public:
+    public: 
     bool operator() (ListNode* a, ListNode* b){
         return a->val > b->val;
     }
 };
 class Solution {
 public:
+    
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         int k = lists.size();
         if(k == 0) return NULL;
 
         priority_queue<ListNode*, vector<ListNode*>, compare> mini;
-        for(int i=0; i<k; i++){
 
+        for(int i=0; i<k; i++){
             if(lists[i] != NULL) mini.push(lists[i]);
         }
+
         ListNode* head = NULL;
         ListNode* tail = NULL;
+
         while(!mini.empty()){
-            ListNode* temp = mini.top();
+            ListNode* top = mini.top();
             mini.pop();
 
-            if(temp->next != NULL) mini.push(temp->next);
+            if(top->next) mini.push(top->next);
 
             if(head == NULL){
-                head = temp;
-                tail = temp;
+                head = top;
+                tail = top;
             }
             else{
-                tail->next = temp;
-                tail = temp;
+                tail->next = top;
+                tail = top;
             }
         }
+
         return head;
     }
 };
