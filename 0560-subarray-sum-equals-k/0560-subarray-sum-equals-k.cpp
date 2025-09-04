aed368f87cf34c9f3e312ac4d34c46451a9f18f2
Jalaj Dabas <jalajdabas4@gmail.com>
2025-09-04 18:41:46 +0530

    Time: 42 ms (66.97%), Space: 47 MB (34.51%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -9,17 +9,18 @@ public:
             sum += nums[i];
             prefix[i] = sum;
         }
-        int ans = 0;
 
         unordered_map<int, int> mp;
+        int ans = 0;
 
         for(int i=0; i<n; i++){
             if(prefix[i] == k) ans++;
 
-            if(mp.find(prefix[i]-k) != mp.end()) ans += mp[prefix[i]-k];
+            if (mp.find(prefix[i]-k) != mp.end()) ans += mp[prefix[i]-k];
 
             mp[prefix[i]]++;
         }
         return ans;
+
     }
 };
