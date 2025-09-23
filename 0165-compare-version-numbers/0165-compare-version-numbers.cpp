c503fddc61abccecf1dedc3109493a7f2aad511f
Jalaj Dabas <jalajdabas4@gmail.com>
2025-09-23 11:39:25 +0530

    Time: 0 ms (100%), Space: 8.2 MB (69.84%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -7,27 +7,23 @@ public:
         int x1 = 0;
         int x2 = 0;
 
-        for(int i=0, j=0; i<v1 || j<v2; i++,j++){
-
-            while(i < v1 && version1[i] != '.'){
-                x1 = x1*10 + (version1[i] - '0');
+        for(int i=0, j=0; i < v1 || j< v2; i++,j++){
+            while(i<v1 && version1[i] != '.'){
+                x1 = 10*x1 + (version1[i] - '0');
                 i++;
             }
 
-            while(j < v2 && version2[j] != '.'){
-                x2 = x2*10 + (version2[j] - '0');
+            while(j<v2 && version2[j] != '.'){
+                x2 = 10*x2 + (version2[j] - '0');
                 j++;
             }
 
-            if(x1 < x2){
-                return -1;
-            }
-            else if(x1 > x2) return 1;
+            if(x1 < x2) return -1;
+            if(x1 > x2) return 1;
 
             x1 = 0;
             x2 = 0;
         }
         return 0;
-
     }
 };
