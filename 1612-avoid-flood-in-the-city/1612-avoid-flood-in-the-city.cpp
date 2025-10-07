ff750322c49019a8b6120d66a160a0cedab18ae3
Jalaj Dabas <jalajdabas4@gmail.com>
2025-10-07 13:46:32 +0530

    Time: 119 ms (68.93%), Space: 114.6 MB (72.6%) - LeetHub

---
+++ 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> avoidFlood(vector<int>& rains) {
+        int n = rains.size();
+        unordered_map<int, int> mp;
+        set<int> index;
+        vector<int> ans(n, 0);
+
+
+        for(int i=0; i<n; i++){
+
+            if(rains[i] == 0){
+                index.insert(i);
+                continue;
+            }
+
+            if(mp.find(rains[i]) != mp.end()){
+                if(index.size() == 0){
+                    return {};
+                }
+
+                else{
+                    auto it= index.lower_bound(mp[rains[i]]);
+
+                    if(it == index.end()) return {};
+
+                    ans[*it] = rains[i];
+                    index.erase(it);
+                }
+
+
+            }
+
+            ans[i] = -1;
+            mp[rains[i]] = i;
+        }
+
+        for(int i=0; i<n; i++){
+            if(ans[i] == 0){
+               ans[i] = 1;
+            }
+        }
+        return ans;
+    }
+};
